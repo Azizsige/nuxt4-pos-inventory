469fb427dec5b8d11d0b45379b6db2b4c362ff59
Muhammad Aziz Sige Kurniawan <46107051+Azizsige@users.noreply.github.com>
2026-04-23 12:40:26 +0700

    Update footer copyright information

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -222,7 +222,7 @@
     >
       <div class="max-w-7xl mx-auto px-4 text-center text-sm text-gray-500">
         &copy; {{ new Date().getFullYear() }} Toko Seragam Hildamr. Sistem
-        Informasi POS & Katalog by [Nama Lu].
+        Informasi POS & Katalog by GarisKode.
       </div>
     </footer>
   </div>
